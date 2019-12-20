5e56d7805869acb141ef9d2f21d42ff052e5fb12
songdn <songdn@tudoutujiu.com>
2019-12-20 18:34:57 +0800

    远程配置

---
+++ microservicecloud-config-client-3355/src/main/java/com/sweet/springcloud/ConfigClient_3355_StartCloudApp.java
@@ -0,0 +1,15 @@
+package com.sweet.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 宋德能
+ * @date 2019年12月20日---下午 3:29
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355_StartCloudApp.class,args);
+    }
+}
